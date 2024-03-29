0ac3d78519397c188724f32b48371de4f34f25f7
Caramajau <caramia0505@gmail.com>
2024-03-29 16:34:38 +0100

    Has choice box with all time zones

--- src/main/java/org/caramajau/view/MainWindow.java
+++ src/main/java/org/caramajau/view/MainWindow.java
@@ -1,14 +1,22 @@
 package org.caramajau.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.caramajau.model.TimeZoneHandler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class MainWindow extends AnchorPane implements Initializable {
     private final Stage primaryStage;
+    @FXML
+    ChoiceBox<String> timeChoiceBox;
 
     public MainWindow(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -17,5 +25,8 @@ public class MainWindow extends AnchorPane implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         primaryStage.setTitle("Time Zone Checker");
+        Set<String> allTimeZonesSet = TimeZoneHandler.getAllTimeZones();
+        ObservableList<String> allTimeZonesList = FXCollections.observableArrayList(allTimeZonesSet);
+        timeChoiceBox.setItems(allTimeZonesList);
     }
 }
