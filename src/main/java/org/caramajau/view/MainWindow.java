5deb8e79b34a3cd6b271a172342a78bc749574b8
Caramajau <caramia0505@gmail.com>
2024-03-30 21:22:40 +0100

    Added support for not writing time and date, and began with time

--- src/main/java/org/caramajau/view/MainWindow.java
+++ src/main/java/org/caramajau/view/MainWindow.java
@@ -4,10 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import org.caramajau.model.TimeZoneHandler;
@@ -15,6 +12,7 @@ import org.caramajau.model.TimeZoneOffsets;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -32,10 +30,16 @@ public class MainWindow extends AnchorPane implements Initializable {
     @FXML
     private DatePicker datePicker;
     @FXML
+    private TextField timeTextField;
+    @FXML
     private Button convertButton;
-
-    private String selectedTimeZone;
-    private LocalDate selectedDate;
+    private static final String NO_SELECTED_TIME_ZONE_STRING = "NaN";
+    // Note to self: "-1" is a valid time zone
+    // Probably means numbers are fine in general
+    // Could probably make the model simpler.
+    private String selectedTimeZone = NO_SELECTED_TIME_ZONE_STRING;
+    private LocalTime selectedTime = LocalTime.of(0, 0);
+    private LocalDate selectedDate = LocalDate.ofEpochDay(0);
 
     public MainWindow(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -66,8 +70,47 @@ public class MainWindow extends AnchorPane implements Initializable {
     }
 
     @FXML
+    private void handleTimeTextField() {
+        String selectedTimeString = timeTextField.getText();
+        String validFormatTimeString = convertToValidFormat(selectedTimeString);
+        selectedTime = LocalTime.parse(selectedTimeString);
+    }
+
+    private String convertToValidFormat(String selectedTimeString) {
+        String timeNumbersString = findTimeNumbersInString(selectedTimeString);
+        return createValidFormat(timeNumbersString);
+
+    }
+
+    private static String findTimeNumbersInString(String selectedTimeString) {
+        StringBuilder timeNumbersBuilder = new StringBuilder();
+        for (char c : selectedTimeString.toCharArray()) {
+            if (Character.isDigit(c)) {
+                timeNumbersBuilder.append(c);
+                if (timeNumbersBuilder.length() == 4) {
+                    break;
+                }
+            }
+        }
+        return timeNumbersBuilder.toString();
+    }
+
+    private static String createValidFormat(String timeNumbersString) {
+        return switch (timeNumbersString.length()) {
+            case 2 -> timeNumbersString + ":00";
+            case 4 -> timeNumbersString.substring(0, 2) + ":" + timeNumbersString.substring(2, 4);
+            default -> throw new IllegalArgumentException("Invalid time format");
+        };
+    }
+
+    @FXML
     private void handleConvertButtonAction() {
-        ZonedDateTime newDate = TimeZoneHandler.convertToCurrentTimeZone(selectedDate, selectedTimeZone);
-        convertedDateLabel.setText("New date: " + newDate);
+        if (!selectedTimeZone.equals(NO_SELECTED_TIME_ZONE_STRING)) {
+            ZonedDateTime newDate = TimeZoneHandler.convertToCurrentTimeZone(selectedDate, selectedTimeZone);
+            convertedDateLabel.setText("New date: " + newDate);
+        } else {
+            convertedDateLabel.setText("Select a time zone!");
+        }
+
     }
 }
