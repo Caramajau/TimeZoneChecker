f835d963ae56a0ed5df752badf7d67958c82d946
Caramajau <caramia0505@gmail.com>
2024-04-01 21:03:52 +0200

    Made clock icon private static final

--- src/main/java/org/caramajau/view/App.java
+++ src/main/java/org/caramajau/view/App.java
@@ -11,8 +11,10 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class App extends Application {
+    // For Maven-JavaFX needs to be relative to where this class is.
     private static final String FXML_FILE_PATH = "main_window.fxml";
     private static final String ICON_FILE_PATH = "clock_icon.png";
+    private static final Image clockIcon = new Image(Objects.requireNonNull(App.class.getResourceAsStream(ICON_FILE_PATH)));
     @Override
     public void start(Stage primaryStage) {
         Parent root;
@@ -20,14 +22,12 @@ public class App extends Application {
 
         // it can generate IOException so surround with try-catch
         try {
-            // For Maven-JavaFX needs to be relative to where this class is.
             loader = new FXMLLoader(getClass().getResource(FXML_FILE_PATH));
             root = loader.load();
-
             Scene scene = new Scene(root);
+
             primaryStage.setTitle("Time Zone Checker");
-            Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream(ICON_FILE_PATH)));
-            primaryStage.getIcons().add(icon);
+            primaryStage.getIcons().add(clockIcon);
             primaryStage.setResizable(false);
             primaryStage.setScene(scene);
             primaryStage.show();
