839d65fcbf3406a91cde5a08d8415bd0038774cf
Caramajau <caramia0505@gmail.com>
2024-04-01 20:56:32 +0200

    Added window icon

--- src/main/java/org/caramajau/view/App.java
+++ src/main/java/org/caramajau/view/App.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
+    private static final String FXML_FILE_PATH = "main_window.fxml";
+    private static final String ICON_FILE_PATH = "clock_icon.png";
     @Override
     public void start(Stage primaryStage) {
         Parent root;
@@ -17,12 +21,13 @@ public class App extends Application {
         // it can generate IOException so surround with try-catch
         try {
             // For Maven-JavaFX needs to be relative to where this class is.
-            String filePath = "main_window.fxml";
-            loader = new FXMLLoader(getClass().getResource(filePath));
+            loader = new FXMLLoader(getClass().getResource(FXML_FILE_PATH));
             root = loader.load();
 
             Scene scene = new Scene(root);
             primaryStage.setTitle("Time Zone Checker");
+            Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream(ICON_FILE_PATH)));
+            primaryStage.getIcons().add(icon);
             primaryStage.setResizable(false);
             primaryStage.setScene(scene);
             primaryStage.show();
