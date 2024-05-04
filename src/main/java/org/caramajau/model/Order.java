c24c731052c8431b074f4ce1e762a5d429f33a2d
Caramajau <caramia0505@gmail.com>
2024-05-04 18:56:19 +0200

    Removed unused classes

--- src/main/java/org/caramajau/model/Order.java
+++
@@ -1,7 +0,0 @@
-package org.caramajau.model;
-
-public enum Order {
-    BEFORE,
-    AFTER,
-    SAME
-}
--- src/main/java/org/caramajau/model/TimeZoneHandler.java
+++ src/main/java/org/caramajau/model/TimeZoneHandler.java
@@ -30,21 +30,6 @@ public class TimeZoneHandler {
         throw new IllegalArgumentException("No time zone found with offset " + offset.getOffset());
     }
 
-    public static Order compareTimeZone(String zoneString1, String zoneString2) {
-        Instant currentTime = Instant.now();
-
-        ZoneOffset offset1 = getZoneOffset(currentTime, zoneString1);
-        ZoneOffset offset2 = getZoneOffset(currentTime, zoneString2);
-
-        if (offset1.compareTo(offset2) > 0) {
-            return Order.AFTER;
-        } else if (offset1.compareTo(offset2) < 0) {
-            return Order.BEFORE;
-        } else {
-            return Order.SAME;
-        }
-    }
-
     private static ZoneOffset getZoneOffset(Instant currentTime, String zoneString) {
         return ZoneId.of(zoneString).getRules().getOffset(currentTime);
     }
